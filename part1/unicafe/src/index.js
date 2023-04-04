4bf26acbc091fd92f7c0925f43437f15638de619
Antonio Martin <luis.martinf89@gmail.com>
2023-04-04 17:07:58 -0500

    solve 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <div>{props.text} {props.value}</div>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good +1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral +1)} text="neutral" />
+      <Button handleClick={() => setBad(bad +1)} text="bad" />
+      <h2>Stadistics</h2>
+      <Display value={good} text="good" />
+      <Display value={neutral} text="neutral" />
+      <Display value={bad} text="bad" />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
