e4588f892bae47de95450af8c36e7dc8d275cf68
Antonio Martin <luis.martinf89@gmail.com>
2023-04-05 13:25:45 -0500

    solve 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,26 +9,39 @@ const Button = (props) => (
 
 const Display = props => <div>{props.text} {props.value}</div>
 
+const Statistics = (props) => {
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  useEffect(() => {
+    let total = props.good - props.bad;
+    let all = props.good + props.neutral + props.bad;
+    setAverage(total / all);
+    setPositive((props.good * 100 / all) + '%');
+  }, [props.good, props.neutral, props.bad])
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <Display value={props.good} text="good" />
+      <Display value={props.neutral} text="neutral" />
+      <Display value={props.bad} text="bad" />
+      <Display value={average} text="average" />
+      <Display value={positive} text="positive" />
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+
 
   const setToValue = (value, set) => () => {
     set(value + 1);
     }
-
-  useEffect(() => {
-    let total = good - bad;
-    let all = good + neutral + bad;
-    let average = total / all;
-    let positive = good * 100 / all;
-    setAverage(average);
-    setPositive(positive + '%');
-  }, [good, neutral, bad]);
 
   return (
     <div>
@@ -36,12 +49,7 @@ const App = () => {
       <Button handleClick={setToValue(good, setGood)} text="good" />
       <Button handleClick={setToValue(neutral, setNeutral)} text="neutral" />
       <Button handleClick={setToValue(bad, setBad)} text="bad" />
-      <h2>Statistics</h2>
-      <Display value={good} text="good" />
-      <Display value={neutral} text="neutral" />
-      <Display value={bad} text="bad" />
-      <Display value={average} text="average" />
-      <Display value={positive} text="positive" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
