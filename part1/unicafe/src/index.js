707585e6be860cae7cd789cb9980daedff45b4e8
Antonio Martin <luis.martinf89@gmail.com>
2023-04-05 11:47:29 -0500

    solve 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = (props) => (
@@ -14,17 +14,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const setToValue = (value, set) => () => {
+    set(value + 1);
+    }
+
+  useEffect(() => {
+    let total = good - bad;
+    let all = good + neutral + bad;
+    let average = total / all;
+    let positive = good * 100 / all;
+    setAverage(average);
+    setPositive(positive + '%');
+  }, [good, neutral, bad]);
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={() => setGood(good +1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral +1)} text="neutral" />
-      <Button handleClick={() => setBad(bad +1)} text="bad" />
-      <h2>Stadistics</h2>
+      <Button handleClick={setToValue(good, setGood)} text="good" />
+      <Button handleClick={setToValue(neutral, setNeutral)} text="neutral" />
+      <Button handleClick={setToValue(bad, setBad)} text="bad" />
+      <h2>Statistics</h2>
       <Display value={good} text="good" />
       <Display value={neutral} text="neutral" />
       <Display value={bad} text="bad" />
+      <Display value={average} text="average" />
+      <Display value={positive} text="positive" />
     </div>
   )
 }
