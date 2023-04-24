e8c8be44f13444fe4b580829a0b62b082ee253a9
Antonio Martin <luis.martinf89@gmail.com>
2023-04-24 11:07:16 -0500

    solve 2.5

--- part2/courseinfo/course.js
+++ part2/courseinfo/course.js
@@ -13,30 +13,28 @@ const Course = ({ course }) => {
     </div>
     )
   };
+
+const Header = ({name}) => {
+  return <h2>{name}</h2>
+};
   
-  <h1>Web development curriculum</h1>
-  const Header = ({name}) => {
-    console.log("nombre: ", name)
-    return <h2>{name}</h2>
-  };
-  
-  const Content = ({parts}) => {
-    return (
-      <div>
-        {parts.map((part) => (
-          <p key={part.id}> {part.name}: {part.exercises} </p>
-        ))}
-      </div>
-    );
-  };
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.id}> {part.name}: {part.exercises} </p>
+      ))}
+    </div>
+  );
+};
   
   
-  const Total = ({parts}) => {
-    return (
-      <p style={{fontWeight:'bold'}}>Number of exercises {parts.map(part => part.exercises).reduce((sum, value) => {
-        return sum + value;
-      }, 0)}</p>
-    )
-  };
+const Total = ({parts}) => {
+  return (
+    <p style={{fontWeight:'bold'}}>Number of exercises {parts.map(part => part.exercises).reduce((sum, value) => {
+      return sum + value;
+    }, 0)}</p>
+  )
+};
   
-  export default Course;
+export default Course;
