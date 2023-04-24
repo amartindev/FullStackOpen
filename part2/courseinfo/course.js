b93b55165b6374fc880094672ee416bd5dc08565
Antonio Martin <luis.martinf89@gmail.com>
2023-04-24 10:11:26 -0500

    solve 2.3

--- part2/courseinfo/course.js
+++ part2/courseinfo/course.js
@@ -1,12 +1,12 @@
 
 const Course = ({ course }) => {
-    return (
-        <div>
-            <Header name = {course.name} />
-            <Content parts = {course.parts} />
-            <Total parts = {course.parts} />
-        </div>
-    )
+  return (
+      <div>
+          <Header name = {course.name} />
+          <Content parts = {course.parts} />
+          <Total parts = {course.parts} />
+      </div>
+  )
 };
 
 const Header = ({name}) => {
@@ -14,20 +14,22 @@ const Header = ({name}) => {
 };
 
 const Content = ({parts}) => {
-    return (
-      <div>
-        {parts.map((part) => (
-          <p key={part.id}> {part.name}: {part.exercises} </p>
-        ))}
-      </div>
-    );
+  return (
+    <div>
+      {parts.map((part) => (
+        <p key={part.id}> {part.name}: {part.exercises} </p>
+      ))}
+    </div>
+  );
 };
 
 
 const Total = ({parts}) => {
-    return (
-      <p style={{fontWeight:'bold'}}>Number of exercises {parts.map(part => part.exercises).reduce((sum, value) => sum + value, 0)}</p>
-    )
-  }
+  return (
+    <p style={{fontWeight:'bold'}}>Number of exercises {parts.map(part => part.exercises).reduce((sum, value) => {
+      return sum + value;
+    }, 0)}</p>
+  )
+};
 
 export default Course;
