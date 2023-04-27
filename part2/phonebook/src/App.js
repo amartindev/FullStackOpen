2dc302158e7228acb497ad746e7bf54b16a30fe8
Antonio Martin <luis.martinf89@gmail.com>
2023-04-27 15:22:03 -0500

    solve 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Phonebook from './components/phonebook'
 import Form from './components/form'
 import Filter from './components/filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber]= useState('')
   const [findPerson, setNewFindPerson] =useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
@@ -52,19 +56,11 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter findPerson={findPerson} findPersonChange={findPersonChange} />
-      <ul>
-        {foundPersons.map(name => (
-            <Phonebook  key={name.name} name={name} />
-          ))}
-      </ul>
+      <Phonebook  persons={foundPersons} />
       <h2>Add a new</h2>
       <Form addName={addName} newName={newName} nameChange={nameChange} newNumber={newNumber} numChange={numChange} />
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(name => (
-          <Phonebook  key={name.name} name={name} />
-        ))}
-      </ul>
+      <Phonebook  persons={persons} />
     </div>
   )
 }
--- part2/phonebook/src/components/phonebook.js
+++ part2/phonebook/src/components/phonebook.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Phonebook = ({ name }) => {
-    return <li>{name.name} {name.number}</li>
-
+const Phonebook = ({ persons }) => {
+    return (
+        <ul>
+        {persons.map(name => (
+            <li key={name.name}>{name.name} {name.number}</li>
+        ))}
+        </ul>
+    )
 }
 
 export default Phonebook
