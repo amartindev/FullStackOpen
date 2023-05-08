f8605dc05f771f2be24a68188a32bf824f8e848f
Antonio Martin <luis.martinf89@gmail.com>
2023-05-08 18:15:21 -0500

    solve 2.17

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import Phonebook from './components/phonebook'
 import Form from './components/form'
 import Filter from './components/filter'
 import noteBook from './services/notes'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -39,6 +40,23 @@ const App = () => {
     setNewNumber('')
   }
 
+  const deleteName = (id) => {
+    if(window.confirm("Delete?")){
+      axios.delete(`http://localhost:3001/persons/${id}`)
+      .then(()=>{
+        noteBook
+        .getAll()
+        .then(initialAgenda => {
+          setPersons(initialAgenda)
+        })
+      })
+      .catch(error => {
+        console.error('Error al realizar la solicitud DELETE', error);
+      });
+    }
+  }
+
+
   const findedPerson = persons.filter(person => person.name.toLowerCase().includes(findPerson.toLowerCase()));
   const foundPersons = findPerson ? findedPerson : [];
 
@@ -59,11 +77,11 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter findPerson={findPerson} findPersonChange={findPersonChange} />
-      <Phonebook  persons={foundPersons} />
+      <Phonebook  persons={foundPersons} deleteName={deleteName}/>
       <h2>Add a new</h2>
       <Form addName={addName} newName={newName} nameChange={nameChange} newNumber={newNumber} numChange={numChange} />
       <h2>Numbers</h2>
-      <Phonebook  persons={persons} />
+      <Phonebook  persons={persons} deleteName={deleteName} />
     </div>
   )
 }
--- part2/phonebook/src/components/phonebook.js
+++ part2/phonebook/src/components/phonebook.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Phonebook = ({ persons }) => {
+const Phonebook = ({ persons, deleteName }) => {
     return (
         <ul>
         {persons.map(name => (
-            <li key={name.name}>{name.name} {name.number}</li>
+            <li key={name.name}>
+                {name.name} {name.number} 
+                <button onClick={() => deleteName(name.id)}> Delete </button> 
+            </li>
         ))}
         </ul>
     )
