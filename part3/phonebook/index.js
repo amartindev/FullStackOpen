930d6b0204c774c3fafaa11d41a58ec9ca8a6c31
Antonio Martin <luis.martinf89@gmail.com>
2023-05-12 18:45:24 -0500

    solve 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,13 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+
+morgan.token("postData", (req) => {
+    if (req.method === "POST") {
+      return JSON.stringify(req.body);
+    }
+  });
 
 let persons = [
     {
